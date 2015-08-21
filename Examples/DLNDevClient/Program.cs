b230aa65c0584eb6947f16b285d306d226d95e84
Wu Yuntao <wyt.brandon@gmail.com>
2015-08-21 18:43:26 +0800

    1, Add a dev client project for debugging

---
+++ Examples/DLNDevClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using DLNSchema;
+using System;
+
+namespace DLNDevClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start.");
+            Console.ReadKey();
+
+            var client = new Client(new ClientHandler());
+
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+
+        #region Client Handler
+
+        class ClientHandler : IClientHandler
+        {
+            public void Log(string msg, params object[] args)
+            {
+                Console.WriteLine(msg, args);
+            }
+
+            public void LogError(string msg, params object[] args)
+            {
+                Console.WriteLine(msg, args);
+            }
+
+            public void OnConnect()
+            {
+                Console.WriteLine("OnConnect");
+            }
+
+            public void OnSync()
+            {
+                Console.WriteLine("OnSync");
+            }
+        }
+        
+        #endregion
+    }
+}
